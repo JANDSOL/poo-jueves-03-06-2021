0cd86dc4f7b32bd6a7cb06eb257fd8133b760bec
Juan Andrade <jpas.juan23@gmail.com>
2021-06-10 18:16:30 -0500

    Constructor and printing method working

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 class Fraction:
     
-    def __init__(self):
-        pass
+    def __init__(self, numerator, denominator):
+        """Method constructor."""
+
+        self.num = numerator
+        self.den = denominator
+
+    def __str__(self):
+        """Printing method."""
+
+        return self.num, '/', self.den
 
 
 if __name__ == '__main__':
